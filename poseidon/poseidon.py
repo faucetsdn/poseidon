0af244f94458b87d46e16e56db0b8ab2d11e852f
cglewis <clewis@iqt.org>
2016-05-20 08:33:07 -0700

    closes #8

--- poseidon/poseidon.py
+++ poseidon/poseidon.py
@@ -25,6 +25,55 @@ Created on 17 May 2016
 import falcon
 import json
 
+from falcon_cors import CORS
+from os import environ
+
+def get_allowed():
+    rest_url = ""
+    if "ALLOW_ORIGIN" in environ:
+        allow_origin = environ["ALLOW_ORIGIN"]
+        host_port = allow_origin.split("//")[1]
+        host = host_port.split(":")[0]
+        port = str(int(host_port.split(":")[1])+1)
+        rest_url = host+":"+port
+    else:
+        allow_origin = ""
+    return allow_origin, rest_url
+
+allow_origin, rest_url = get_allowed()
+cors = CORS(allow_origins_list=[allow_origin])
+public_cors = CORS(allow_all_origins=True)
+
+class SwaggerAPI:
+    """Serve up swagger API"""
+    swagger_file = '/poseidon/poseidon/swagger.yaml'
+    def on_get(self, req, resp):
+        """Handles GET requests"""
+        resp.content_type = 'text/yaml'
+        try:
+            # update mydomain with current running host and port
+            with open(self.swagger_file, 'r') as f:
+                filedata = f.read()
+            newdata = filedata.replace("mydomain", rest_url)
+            with open(self.swagger_file, 'w') as f:
+                f.write(newdata)
+
+            with open(self.swagger_file, 'r') as f:
+               resp.body = f.read()
+        except: # pragma: no cover
+            resp.body = ""
+
+class VersionResource:
+    """Serve up the current version and build information"""
+    def on_get(self, req, resp):
+        """Handles GET requests"""
+        version = {}
+        # TODO
+        # version number
+        # build number (docker container ID)
+        # commit version (git commit ID)
+        resp.body = json.dumps(version)
+
 class QuoteResource:
     """Serve up quotes"""
     def on_get(self, req, resp):
@@ -37,5 +86,7 @@ class QuoteResource:
         resp.body = json.dumps(quote)
 
 # create callable WSGI app instance for gunicorn
-api = falcon.API()
-api.add_route('/quote', QuoteResource())
+api = falcon.API(middleware=[cors.middleware])
+api.add_route('/v1/quote', QuoteResource())
+api.add_route('/v1/version', VersionResource())
+api.add_route('/swagger.yaml', SwaggerAPI())
