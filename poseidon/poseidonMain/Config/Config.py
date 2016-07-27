04c6486faaed3ef11f59280793ac5e4b1c96fadf
d-grossman <dgrossman@iqt.org>
2016-07-27 15:45:27 -0700

    wrong path

--- poseidon/poseidonMain/Config/Config.py
+++ poseidon/poseidonMain/Config/Config.py
@@ -21,7 +21,8 @@ import os
 import urllib2
 
 from poseidon.baseClasses.Main_Action_Base import Main_Action_Base
-CI_TESTING = 'file:///poseidonWork/templates/config.template'
+# CI_TESTING = 'file:///poseidonWork/templates/config.template'
+CI_TESTING = '/poseidonWork / templates / config.template'
 
 
 class Config(Main_Action_Base):
