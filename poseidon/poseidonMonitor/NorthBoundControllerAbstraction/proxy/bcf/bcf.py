4a47b91bc4ed69a608bf57e6a0333998e4881de3
dgrossman <dgrossman@iqt.org>
2016-09-07 16:00:09 -0700

    can poll controller to get state update

--- poseidon/poseidonMonitor/NorthBoundControllerAbstraction/proxy/bcf/bcf.py
+++ poseidon/poseidonMonitor/NorthBoundControllerAbstraction/proxy/bcf/bcf.py
@@ -13,10 +13,10 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-"""
+'''
 Created on 25 July 2016
 @author: kylez,dgrossman
-"""
+'''
 import json
 import logging
 from urlparse import urljoin
@@ -30,17 +30,20 @@ module_logger = logging.getLogger(__name__)
 class BcfProxy(JsonMixin, CookieAuthControllerProxy):
 
     def __init__(self, base_uri, login_resource='auth/login', auth={'user': None, 'password': None}, *args, **kwargs):
-        """
+        '''
         Initializes BcfProxy object.
 
         Example usage:
         bcf = BcfProxy("https://127.0.0.1:8443/api/v1/", auth={"user": "USER", "password": "PASSWORD"})
-        """
+        '''
         super(BcfProxy, self).__init__(
             base_uri, login_resource, auth, *args, **kwargs)
 
     @staticmethod
     def reduce_endpoints(data):
+        '''
+        return only the information needed for the application
+        '''
         ret_list = list()
         for d in data:
             ipa = d.get('ip-address')
@@ -51,9 +54,9 @@ class BcfProxy(JsonMixin, CookieAuthControllerProxy):
         return ret_list
 
     def get_endpoints(self, endpoints_resource='data/controller/applications/bcf/info/endpoint-manager/endpoint'):
-        """
+        '''
         GET list of endpoints from the controller.
-        """
+        '''
         r = self.get_resource(endpoints_resource)
         retval = self.reduce_endpoints(JsonMixin.parse_json(r))
         sout = 'get_endpoints return:{0}'.format(retval)
@@ -61,18 +64,18 @@ class BcfProxy(JsonMixin, CookieAuthControllerProxy):
         return retval
 
     def get_switches(self, switches_resource='data/controller/applications/bcf/info/fabric/switch'):
-        """
+        '''
         GET list of switches from the controller.
-        """
+        '''
         r = self.get_resource(switches_resource)
         retval = BcfProxy.parse_json(r)
         module_logger.debug('get_switches return:{0}'.format(retval))
         return retval
 
     def get_tenants(self, tenant_resource='data/controller/applications/bcf/info/endpoint-manager/tenant'):
-        """
+        '''
         GET list of tenants from the controller.
-        """
+        '''
         r = self.get_resource(tenant_resource)
         retval = BcfProxy.parse_json(r)
         sout = 'get_tenants return:{0}'.format(retval)
@@ -80,9 +83,9 @@ class BcfProxy(JsonMixin, CookieAuthControllerProxy):
         return retval
 
     def get_segments(self, segment_resource='data/controller/applications/bcf/info/endpoint-manager/segment'):
-        """
+        '''
         GET list of segments from the controller.
-        """
+        '''
         r = self.get_resource(segment_resource)
         retval = BcfProxy.parse_json(r)
         sout = 'get_segments return:{0}'.format(retval)
@@ -90,11 +93,11 @@ class BcfProxy(JsonMixin, CookieAuthControllerProxy):
         return retval
 
     def get_span_fabric(self, span_name=None, span_fabric_resource='data/controller/applications/bcf/span-fabric'):
-        """
+        '''
         GET list of span fabric configuration.
 
         use this to task mirror_traffic
-        """
+        '''
         if span_name:
             span_fabric_resource = ''.join(
                 [span_fabric_resource, '[name="%s"]' % span_name])
@@ -105,7 +108,7 @@ class BcfProxy(JsonMixin, CookieAuthControllerProxy):
         return retval
 
     def shutdown_endpoint(self, tenant, segment, endpoint_name, mac=None, shutdown=True, shutdown_resource="data/controller/applications/bcf/tenant[name=\"%s\"]/segment[name=\"%s\"]/endpoint"):
-        """
+        '''
         PUT to jail (i.e. isolate) an endpoint from the network.
 
         Jail with shutdown=True, unjail by shutdown=False.
@@ -115,7 +118,7 @@ class BcfProxy(JsonMixin, CookieAuthControllerProxy):
 
         if there is an endpoint name, only need endpoint_name
 
-        """
+        '''
         subs = (tenant, segment)
         resource = shutdown_resource % subs
         uri = urljoin(self.base_uri, resource)
@@ -129,12 +132,12 @@ class BcfProxy(JsonMixin, CookieAuthControllerProxy):
         return retval
 
     def mirror_traffic(self, seq, mirror=True, span_name='vent', fabric_span_endpoint="data/controller/applications/bcf/span-fabric[name=\"%s\"]", **target_kwargs):
-        """
+        '''
 
         If mirror=True, PUT to apply the filter rules specified by target_kwargs to a specified span fabric (vent by default). For example, bcf.mirror_traffic(seq=1, tenant="TENANT", segment="SEGMENT") will apply a rule with seq=1 filtering all traffic matching tenant TENANT and segment SEGMENT. If a rule with seq=1 already exists, it will be overwritten.
 
         If mirror=False, PUT to delete the rule with specified seq. If no such rule exists, this call does nothing.
-        """
+        '''
         resource = fabric_span_endpoint % span_name
         uri = urljoin(self.base_uri, resource)
         data = self.get_span_fabric()[0]  # first element is vent span rule
