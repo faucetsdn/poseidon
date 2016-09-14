8e11f9a5ec376a5ea0e72112ddb3ec5a5c5bc75e
lanhamt <tlanham@cs.stanford.edu>
2016-09-14 11:14:08 -0700

    fix for nprocessor collector

--- scripts/add_records.py
+++ scripts/add_records.py
@@ -17,13 +17,12 @@ def get_host():
             return host
 
 
-def add_docs(storage_host, db, coll, port='28000'):
+def add_docs(host, db, coll, doc, port='28000'):
     try:
         add_doc_uri = 'http://' + host + ':' + port + \
                       '/v1/storage/add_one_doc/{database}/{collection}'.format(
                        database=db,
                        collection=coll)
-        doc = {}
         resp = requests.post(add_doc_uri, json=doc)
         print 'doc id is: ', resp.text
         doc_ids.append(resp.text)
@@ -31,6 +30,19 @@ def add_docs(storage_host, db, coll, port='28000'):
         print str(e)
 
 
+def get_doc(doc_id, host, port, db, coll):
+    try:
+        uri = 'http://' + host + ':' + port + \
+              '/v1/storage/doc/{database}/{collection}/{doc_id}'.format(
+               database=db,
+               collection=coll,
+               doc_id=doc_id)
+        resp = requests.get(uri)
+        print resp.text
+    except Exception, e:
+        print str(e)
+
+
 if __name__ == '__main__':
     host = get_host()
-    add_docs(host, 'poseidon_records', 'netgraph_beta')
+    #print doc_ids
