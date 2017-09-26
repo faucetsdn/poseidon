3848b5ffcb3ea01460e2a5f22c5b3a2f16f2ee53
d grossman <dgrossman@xyxxl.com>
2017-09-26 11:53:28 -0700

    fix codacy issues

--- poseidon/poseidonMonitor/poseidonMonitor.py
+++ poseidon/poseidonMonitor/poseidonMonitor.py
@@ -233,9 +233,10 @@ class Monitor(object):
         ostr = '{0}:config:{1}'.format(self.mod_name, self.mod_configuration)
         self.logger.debug(ostr)
 
-    def update_next_state(self, endpoint_states, rabbit_transitions):
+    def update_next_state(self, rabbit_transitions):
         next_state = None
         current_state = None
+        endpoint_state = self.uss.return_endpoint_state()
         for my_hash in endpoint_states.keys():
             my_dict = endpoint_states[my_hash]
             current_state = my_dict['state']
@@ -270,7 +271,7 @@ class Monitor(object):
             self.logger.debug('failed to start vent collector' + str(e))
 
     def get_rabbit_message(self, item):
-        self.logger.debug('rabbit_message:{1}'.format(found_work, item))
+        self.logger.debug('rabbit_message:{1}'.format(item))
         routing_key, my_obj = item
         my_obj = json.loads(my_obj)
         ret_val = {}
@@ -297,8 +298,7 @@ class Monitor(object):
 
             eps = self.uss.return_endpoint_state()
 
-            state_transitions = self.update_next_state(
-                eps, rabbit_transitions)
+            state_transitions = self.update_next_state(rabbit_transitions)
 
             self.print_endpoint_state(eps)
 
