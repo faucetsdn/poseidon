674b7300fddd6e15fdab4e078d89a030f695aa00
Misha <mshelemetev@gmail.com>
2017-09-30 14:05:28 -0400

    test_signal_handler

--- poseidon/poseidonMonitor/test_poseidonMonitor.py
+++ poseidon/poseidonMonitor/test_poseidonMonitor.py
@@ -14,10 +14,10 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 """
-Test module for NorthBoundControllerAbstraction.py
+Test module for poseidonMonitor.py
 
 Created on 28 June 2016
-@author: dgrossman
+@author: dgrossman, MShel
 """
 import pytest
 
@@ -25,3 +25,36 @@ from poseidon.baseClasses.Logger_Base import Logger
 from poseidon.poseidonMonitor.poseidonMonitor import Monitor
 
 module_logger = Logger.logger
+
+def test_signal_handler():
+    class MockRabbitConnection:
+        connection_closed = False
+
+        def close(self):
+            self.connection_closed = True
+            return True
+
+    class MockMonitor(Monitor):
+        # no need to init the monitor
+        def __init__(self):
+            pass
+
+    class MockScheduele:
+
+        call_log = []
+
+        def __init__(self):
+            self.jobs = ['job1', 'job2', 'job3']
+
+        def cancel_job(self, job):
+            self.call_log.append(job + ' cancelled')
+            return job + ' cancelled'
+
+    mock_monitor = MockMonitor()
+    mock_monitor.schedule = MockScheduele()
+    mock_monitor.rabbit_channel_connection_local = MockRabbitConnection()
+    mock_monitor.logger = module_logger
+    # signal handler seem to simply exit and kill all the jobs no matter what we pass
+    mock_monitor.signal_handler(None, None)
+    assert ['job1 cancelled','job2 cancelled','job3 cancelled'] == mock_monitor.schedule.call_log
+    assert True == mock_monitor.rabbit_channel_connection_local.connection_closed
