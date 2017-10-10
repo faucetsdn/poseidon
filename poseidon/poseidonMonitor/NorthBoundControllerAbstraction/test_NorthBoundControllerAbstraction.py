3611492ce3e2bf1edf8d3ea43a32bc0033e7e35c
d grossman <dgrossman@xyxxl.com>
2017-10-10 11:53:21 -0700

    #241 test_update_endpoint_state

--- poseidon/poseidonMonitor/NorthBoundControllerAbstraction/test_NorthBoundControllerAbstraction.py
+++ poseidon/poseidonMonitor/NorthBoundControllerAbstraction/test_NorthBoundControllerAbstraction.py
@@ -22,5 +22,29 @@ Created on 28 June 2016
 """
 
 from poseidon.baseClasses.Logger_Base import Logger
+from poseidon.poseidonMonitor.NorthBoundControllerAbstraction.NorthBoundControllerAbstraction import controller_interface
+import json
 
 module_logger = Logger.logger
+
+
+def test_update_endpoint_state():
+
+    class mybcf():
+        def __init__(self):
+            pass
+
+        def format_endpoints(self, data):
+            a = [{'ip-address': '10.0.0.1', 'mac': 'f8:b1:56:fe:f2:de', 'segment': 'prod', 'tenant': 'FLOORPLATE', 'name': None},
+                 {'ip-address': '10.0.0.2', 'mac': '20:4c:9e:5f:e3:c3', 'segment': 'to-core-router', 'tenant': 'EXTERNAL', 'name': None}]
+
+            return a
+
+        def get_endpoints(self):
+            pass
+
+    uss = controller_interface.get_endpoint('Update_Switch_State')
+    uss.bcf = mybcf()
+    output = json.loads(uss.update_endpoint_state())
+    correct_output =  json.loads('{"service": "NorthBoundControllerAbstraction:Update_Switch_State", "times": 0, "machines": [{"ip-address": "10.0.0.1", "mac": "f8:b1:56:fe:f2:de", "segment": "prod", "tenant": "FLOORPLATE", "name": null}, {"ip-address": "10.0.0.2", "mac": "20:4c:9e:5f:e3:c3", "segment": "to-core-router", "tenant": "EXTERNAL", "name": null}], "resp": "ok"}')
+    assert str(output) == str(correct_output)
