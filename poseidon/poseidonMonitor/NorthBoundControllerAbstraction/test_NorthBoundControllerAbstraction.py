9b2eba403f3b8eea8accd3987604ba30b9cbf6aa
d grossman <dgrossman@xyxxl.com>
2017-10-11 10:31:36 -0700

    Test creation: find_new_machines #239

--- poseidon/poseidonMonitor/NorthBoundControllerAbstraction/test_NorthBoundControllerAbstraction.py
+++ poseidon/poseidonMonitor/NorthBoundControllerAbstraction/test_NorthBoundControllerAbstraction.py
@@ -23,6 +23,7 @@ Created on 28 June 2016
 
 from poseidon.baseClasses.Logger_Base import Logger
 from poseidon.poseidonMonitor.NorthBoundControllerAbstraction.NorthBoundControllerAbstraction import controller_interface
+from poseidon.poseidonMonitor.NorthBoundControllerAbstraction.NorthBoundControllerAbstraction import Update_Switch_State
 import json
 
 module_logger = Logger.logger
@@ -46,5 +47,28 @@ def test_update_endpoint_state():
     uss = controller_interface.get_endpoint('Update_Switch_State')
     uss.bcf = mybcf()
     output = json.loads(uss.update_endpoint_state())
-    correct_output =  json.loads('{"service": "NorthBoundControllerAbstraction:Update_Switch_State", "times": 0, "machines": [{"ip-address": "10.0.0.1", "mac": "f8:b1:56:fe:f2:de", "segment": "prod", "tenant": "FLOORPLATE", "name": null}, {"ip-address": "10.0.0.2", "mac": "20:4c:9e:5f:e3:c3", "segment": "to-core-router", "tenant": "EXTERNAL", "name": null}], "resp": "ok"}')
+    correct_output = json.loads(
+        '{"service": "NorthBoundControllerAbstraction:Update_Switch_State", "times": 0, "machines": [{"ip-address": "10.0.0.1", "mac": "f8:b1:56:fe:f2:de", "segment": "prod", "tenant": "FLOORPLATE", "name": null}, {"ip-address": "10.0.0.2", "mac": "20:4c:9e:5f:e3:c3", "segment": "to-core-router", "tenant": "EXTERNAL", "name": null}], "resp": "ok"}')
     assert str(output) == str(correct_output)
+
+
+def test_find_new_machines_first_time():
+    uss = Update_Switch_State()
+    uss.first_time = True
+    machines = [{'ip-address': '10.0.0.101', 'mac': 'f8:b1:56:fe:f2:de', 'segment': 'prod', 'tenant': 'FLOORPLATE', 'name': None},
+                {'ip-address': '10.0.0.99', 'mac': '20:4c:9e:5f:e3:c3', 'segment': 'to-core-router', 'tenant': 'EXTERNAL', 'name': None}]
+    uss.find_new_machines(machines)
+    answer = dict({"d502caea3609d553ab16a00c554f0602c1419f58": {"state": "KNOWN", "next-state": "NONE", "endpoint": {"ip-address": "10.0.0.101", "mac": "f8:b1:56:fe:f2:de", "segment": "prod", "tenant": "FLOORPLATE", "name": None}},
+                   "3da53a95ae5d034ae37b539a24370260a36f8bb2": {"state": "KNOWN", "next-state": "NONE", "endpoint": {"ip-address": "10.0.0.99", "mac": "20:4c:9e:5f:e3:c3", "segment": "to-core-router", "tenant": "EXTERNAL", "name": None}}})
+    assert str(answer) == str(dict(uss.endpoint_states))
+
+
+def test_file_new_machines_later():
+    uss = Update_Switch_State()
+    uss.first_time = False
+    machines = [{'ip-address': '10.0.0.101', 'mac': 'f8:b1:56:fe:f2:de', 'segment': 'prod', 'tenant': 'FLOORPLATE', 'name': None},
+                {'ip-address': '10.0.0.99', 'mac': '20:4c:9e:5f:e3:c3', 'segment': 'to-core-router', 'tenant': 'EXTERNAL', 'name': None}]
+    answer = dict({'d502caea3609d553ab16a00c554f0602c1419f58': {'state': 'UNKNOWN', 'next-state': 'NONE', 'endpoint': {'ip-address': '10.0.0.101', 'mac': 'f8:b1:56:fe:f2:de', 'segment': 'prod', 'tenant': 'FLOORPLATE', 'name': None}},
+                   '3da53a95ae5d034ae37b539a24370260a36f8bb2': {'state': 'UNKNOWN', 'next-state': 'NONE', 'endpoint': {'ip-address': '10.0.0.99', 'mac': '20:4c:9e:5f:e3:c3', 'segment': 'to-core-router', 'tenant': 'EXTERNAL', 'name': None}}})
+    uss.find_new_machines(machines)
+    assert str(answer) == str(dict(uss.endpoint_states))
