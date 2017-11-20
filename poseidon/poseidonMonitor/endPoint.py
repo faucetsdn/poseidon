90512f67a61a65c9604d6dd95ea7f8e68f7ac8aa
d grossman <dgrossman@xyxxl.com>
2017-11-20 15:33:28 -0800

    EndPoint

--- poseidon/poseidonMonitor/endPoint.py
+++ poseidon/poseidonMonitor/endPoint.py
@@ -30,7 +30,10 @@ class EndPoint:
         self.prev_state = prev_state
         self.state = state
         self.next_state = next_state
-        self.endpoint_data = dict(endpoint_data)
+        if endpoint_data is not None:
+            self.endpoint_data = dict(endpoint_data)
+        else:
+            self.endpoint_data = {}
 
     def make_hash(self):
         ''' hash the metadata in a sane way '''
