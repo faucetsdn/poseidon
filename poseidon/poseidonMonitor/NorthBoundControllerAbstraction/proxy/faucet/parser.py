87e9ad116bfa5e4c6711a32ed13a9957f4122f6e
cglewis <clewis@iqt.org>
2018-01-24 09:59:43 -0800

    mount log and config files; use defaults

--- poseidon/poseidonMonitor/NorthBoundControllerAbstraction/proxy/faucet/parser.py
+++ poseidon/poseidonMonitor/NorthBoundControllerAbstraction/proxy/faucet/parser.py
@@ -40,13 +40,13 @@ class Parser:
 
     def config(self, config_file, action, port, switch):
         switch_found = None
-        if config_file:
-            # TODO check for other files
-            stream = open(config_file, 'r')
-            obj_doc = yaml.safe_load(stream)
-            stream.close()
-        else:
-            return False
+        if not config_file:
+            # default to FAUCET default
+            config_file = '/etc/ryu/faucet/faucet.yaml'
+        # TODO check for other files
+        stream = open(config_file, 'r')
+        obj_doc = yaml.safe_load(stream)
+        stream.close()
 
         if action == 'mirror':
             ok = True
@@ -123,27 +123,29 @@ class Parser:
 
     def log(self, log_file):
         mac_table = {}
+        if not log_file:
+            # default to FAUCET default
+            log_file = '/var/log/ryu/faucet/faucet.log'
         # NOTE very fragile, prone to errors
-        if log_file:
-            with open(log_file, 'r') as f:
-                for line in f:
-                    if 'L2 learned' in line:
-                        learned_mac = line.split()
-                        data = {'ip-address': learned_mac[16][0:-1],
-                                'ip-state': 'L2 learned',
-                                'mac': learned_mac[10],
-                                'segment': learned_mac[7][1:-1],
-                                'port': learned_mac[19],
-                                'tenant': learned_mac[21] + learned_mac[22]}
-                        if learned_mac[10] in mac_table:
-                            dup = False
-                            for d in mac_table[learned_mac[10]]:
-                                if data == d:
-                                    dup = True
-                            if dup:
-                                mac_table[learned_mac[10]].remove(data)
-                            mac_table[learned_mac[10]].insert(0, data)
-                        else:
-                            mac_table[learned_mac[10]] = [data]
+        with open(log_file, 'r') as f:
+            for line in f:
+                if 'L2 learned' in line:
+                    learned_mac = line.split()
+                    data = {'ip-address': learned_mac[16][0:-1],
+                            'ip-state': 'L2 learned',
+                            'mac': learned_mac[10],
+                            'segment': learned_mac[7][1:-1],
+                            'port': learned_mac[19],
+                            'tenant': learned_mac[21] + learned_mac[22]}
+                    if learned_mac[10] in mac_table:
+                        dup = False
+                        for d in mac_table[learned_mac[10]]:
+                            if data == d:
+                                dup = True
+                        if dup:
+                            mac_table[learned_mac[10]].remove(data)
+                        mac_table[learned_mac[10]].insert(0, data)
+                    else:
+                        mac_table[learned_mac[10]] = [data]
         return mac_table
 
