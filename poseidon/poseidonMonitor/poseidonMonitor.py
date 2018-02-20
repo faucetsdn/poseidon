ed982f5630df1a03143a0ae23f306b33272c1a90
cglewis <clewis@iqt.org>
2018-02-20 15:55:29 -0800

    fix endpoint wrapper changes

--- poseidon/poseidonMonitor/poseidonMonitor.py
+++ poseidon/poseidonMonitor/poseidonMonitor.py
@@ -288,20 +288,20 @@ class Monitor(object):
                     self.logger.debug('ML_DECISION:{0}'.format(ml_decision))
                     if current_state == 'REINVESTIGATING':
                         if ml_decision == 'normal':
-                            self.uss.change_endpoint_nextstate(
+                            endpoint.change_endpoint_nextstate(
                                 my_hash, 'KNOWN')
                             self.logger.debug('REINVESTIGATION Making KNOWN')
                         else:
-                            self.uss.change_endpoint_nextstate(
+                            endpoint.change_endpoint_nextstate(
                                 my_hash, 'UNKNOWN')
                             self.logger.debug('REINVESTIGATION Making UNKNOWN')
                     if current_state == 'MIRRORING':
                         if ml_decision == 'normal':
-                            self.uss.change_endpoint_nextstate(
+                            endpoint.change_endpoint_nextstate(
                                 my_hash, 'KNOWN')
                             self.logger.debug('MIRRORING Making KNOWN')
                         else:
-                            self.uss.change_endpoint_nextstate(
+                            endpoint.change_endpoint_nextstate(
                                 my_hash, 'SHUTDOWN')
                             self.logger.debug('MIRRORING Making SHUTDOWN')
 
