501c2911439ceeda9ef6e2d55cafaa52b190de0d
cglewis <clewis@iqt.org>
2018-03-15 10:07:02 -0700

    add initial api app

---
+++ api/README.md
@@ -0,0 +1,3 @@
+Simple RESTful API for queries information about nodes that Poseidon detects.
+
+For a list of available API endpoints to the service browse to `/v1` where the service is running.
---
+++ api/app/app.py
@@ -0,0 +1,12 @@
+import falcon
+
+from falcon_cors import CORS
+from .routes import routes, version
+
+
+cors = CORS(allow_all_origins=True)
+api = application = falcon.API(middleware=[cors.middleware])
+
+r = routes()
+for route in r:
+    api.add_route(version()+route, r[route])
