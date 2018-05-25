532a604c45d72be65b152c7856315d5d11a209ad
cglewis <clewis@iqt.org>
2018-05-24 17:39:21 -0700

    more tests for the api

--- api/tests/test_app.py
+++ api/tests/test_app.py
@@ -26,12 +26,37 @@ def test_setup_redis():
     r.sadd('ip_addresses', '10.0.0.1')
     r.sadd('ip_addresses', '10.0.0.2')
     r.sadd('ip_addresses', '10.0.0.3')
+    r.sadd('ip_addresses', '2601:645:8200:a571:18fd:6640:9cd9:10d3')
     r.hmset('10.0.0.1',
-            {'poseidon_hash': '6cd09124a66ef1bbc72c1aff4e333766d3533f81'})
+            {'poseidon_hash': '6cd09124a66ef1bbc72c1aff4e333766d3533f81',
+             'timestamps': "['1527208227']",
+             'short_os': 'Mac'})
     r.hmset('10.0.0.2',
-            {'poseidon_hash': '6cd09124a66ef1bbc72c1aff4e333766d3533f82'})
+            {'poseidon_hash': '6cd09124a66ef1bbc72c1aff4e333766d3533f82',
+             'timestamps': "['1527208220', '1527208227']",
+             'short_os': 'Windows'})
     r.hmset('10.0.0.3',
-            {'poseidon_hash': '6cd09124a66ef1bbc72c1aff4e333766d3533f83'})
+            {'poseidon_hash': '6cd09124a66ef1bbc72c1aff4e333766d3533f83',
+             'timestamps': "['1527208220', '1527208228']",
+             'short_os': 'Linux'})
+    r.hmset('10.0.0.1_1527208227', {'labels': ["Developer workstation",
+                                               "Unknown",
+                                               "Smartphone"],
+                                    'confidences': [0.6065386838895384,
+                                                    0.3487681867266965,
+                                                    0.015645883198622094]})
+    r.hmset('10.0.0.2_1527208227', {'labels': ["Developer workstation",
+                                               "Unknown",
+                                               "Smartphone"],
+                                    'confidences': [0.6065386838895384,
+                                                    0.3487681867266965,
+                                                    0.015645883198622094]})
+    r.hmset('10.0.0.3_1527208228', {'labels': ["Developer workstation",
+                                               "Unknown",
+                                               "Smartphone"],
+                                    'confidences': [0.6065386838895384,
+                                                    0.3487681867266965,
+                                                    0.015645883198622094]})
     r.hmset('6cd09124a66ef1bbc72c1aff4e333766d3533f81',
             {"transition_time": "1524623228.1019075",
              "prev_state": "None",
@@ -40,7 +65,8 @@ def test_setup_redis():
                                 'ip-address': '10.0.0.1', \
                                 'segment': '1', \
                                 'port': '1', \
-                                'tenant': 'VLAN100'}",
+                                'tenant': 'VLAN100', \
+                                'state': 'KNOWN'}",
              "next_state": "REINVESTIGATING",
              "state": "KNOWN"})
     r.hmset('6cd09124a66ef1bbc72c1aff4e333766d3533f82',
@@ -51,7 +77,8 @@ def test_setup_redis():
                                 'ip-address': '10.0.0.2', \
                                 'segment': '1', \
                                 'port': '1', \
-                                'tenant': 'VLAN100'}",
+                                'tenant': 'VLAN100', \
+                                'state': 'KNOWN'}",
              "next_state": "REINVESTIGATING",
              "state": "KNOWN"})
     r.hmset('6cd09124a66ef1bbc72c1aff4e333766d3533f83',
@@ -62,7 +89,8 @@ def test_setup_redis():
                                 'ip-address': '10.0.0.3', \
                                 'segment': '1', \
                                 'port': '1', \
-                                'tenant': 'VLAN100'}",
+                                'tenant': 'VLAN100', \
+                                'state': 'KNOWN'}",
              "next_state": "REINVESTIGATING",
              "state": "KNOWN"})
 
