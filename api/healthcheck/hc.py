2d639daba04c2e4b90e4c61f928c78211b00856e
cglewis <clewis@iqt.org>
2018-06-25 19:09:46 -0700

    add simple healthcheck for api

---
+++ api/healthcheck/hc.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from healthcheck import HealthCheck, EnvironmentDump
+
+app = Flask(__name__)
+
+health = HealthCheck(app, "/healthcheck")
+envdump = EnvironmentDump(app, "/environment")
+
+
+def application_data():
+    return {"maintainer": "Charlie Lewis",
+            "git_repo": "https://github.com/CyberReboot/poseidon",
+            "app": "poseidon-api"}
+
+envdump.add_section("application", application_data)
