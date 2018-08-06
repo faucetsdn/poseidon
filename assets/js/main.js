ee46bbc9121fc15277ff789e9dee6beac8f91354
cglewis <clewis@iqt.org>
2018-08-06 10:36:33 -0700

    stub

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+
+    /* ===== Stickyfill ===== */
+    /* Ref: https://github.com/wilddeer/stickyfill */
+    // Add browser support to position: sticky
+    var elements = $('.sticky');
+    Stickyfill.add(elements);
+
+
+    /* Hack related to: https://github.com/twbs/bootstrap/issues/10236 */
+    $(window).on('load resize', function() {
+        $(window).trigger('scroll');
+    });
+
+    /* Activate scrollspy menu */
+    $('body').scrollspy({target: '#doc-menu', offset: 100});
+
+    /* Smooth scrolling */
+	$('a.scrollto').on('click', function(e){
+        //store hash
+        var target = this.hash;
+        e.preventDefault();
+		$('body').scrollTo(target, 800, {offset: 0, 'axis':'y'});
+
+	});
+
+
+    /* ======= jQuery Responsive equal heights plugin ======= */
+    /* Ref: https://github.com/liabru/jquery-match-height */
+
+    // $('#cards-wrapper .item-inner').matchHeight();
+    // $('#showcase .card').matchHeight();
+
+    /* Bootstrap lightbox */
+    /* Ref: http://ashleydw.github.io/lightbox/ */
+
+    $(document).delegate('*[data-toggle="lightbox"]', 'click', function(e) {
+        e.preventDefault();
+        $(this).ekkoLightbox();
+    });
+
+
+});
