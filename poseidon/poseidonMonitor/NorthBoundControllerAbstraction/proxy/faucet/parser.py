f4a780849408bc6cc2a2bfac5f2649cba5978277
cglewis <clewis@iqt.org>
2018-08-21 12:50:49 -0700

    convert to str

--- poseidon/poseidonMonitor/NorthBoundControllerAbstraction/proxy/faucet/parser.py
+++ poseidon/poseidonMonitor/NorthBoundControllerAbstraction/proxy/faucet/parser.py
@@ -146,7 +146,8 @@ class Parser:
             data['active'] = 1
             self.poseidon_logger.debug(
                 'mac: ' + message['L2_LEARN']['eth_src'])
-            self.poseidon_logger.debug('mac table before: ' + self.mac_table)
+            self.poseidon_logger.debug(
+                'mac table before: ' + str(self.mac_table))
             if message['L2_LEARN']['eth_src'] in self.mac_table:
                 dup = False
                 for d in self.mac_table[message['L2_LEARN']['eth_src']]:
@@ -157,7 +158,8 @@ class Parser:
                 self.mac_table[message['L2_LEARN']['eth_src']].insert(0, data)
             else:
                 self.mac_table[message['L2_LEARN']['eth_src']] = [data]
-            self.poseidon_logger.debug('mac table after: ' + self.mac_table)
+            self.poseidon_logger.debug(
+                'mac table after: ' + str(self.mac_table))
         elif 'L2_EXPIRE' in message:
             self.poseidon_logger.debug(
                 'got faucet message for l2_expire: {0}'.format(message))
