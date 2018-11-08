fe766d689e169258f4391059b82ed01115710b46
cglewis <clewis@iqt.org>
2018-11-07 17:19:58 -0800

    closes #785

--- poseidon/poseidonMonitor/poseidonMonitor.py
+++ poseidon/poseidonMonitor/poseidonMonitor.py
@@ -624,8 +624,16 @@ class Monitor(object):
                 # cleanup endpoints that are no longer active
                 for my_hash in dup_eps_state:
                     if eps.state[my_hash].endpoint_data['active'] == 0:
-                        eps.change_endpoint_state(my_hash, new_state='INACTIVE')
+                        eps.change_endpoint_state(
+                            my_hash, new_state='INACTIVE')
                         eps.change_endpoint_nextstate(my_hash, 'NONE')
+                    elif eps.get_endpoint_state(my_hash) == 'INACTIVE':
+                        self.poseidon_logger.info(
+                            'Updating: {0}:{1}->{2}'.format(my_hash,
+                                                            current_state,
+                                                            'REINVESTIGATING'))
+                        eps.change_endpoint_nextstate(
+                            my_hash, 'REINVESTIGATING')
 
                 # make the transitions
                 for endpoint_hash in dup_eps_state:
