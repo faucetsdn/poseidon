1c1f4a73859f9eabc371d58086a4e7753972657f
cglewis <clewis@iqt.org>
2018-12-12 08:31:39 -0800

    wrong place to initialize

--- tests/test_prometheus.py
+++ tests/test_prometheus.py
@@ -11,7 +11,6 @@ def test_Prometheus():
     Tests Prometheus
     """
     p = Prometheus.get_metrics()
-    p.initialize_metrics()
     hosts = [{'active': 0, 'record_source': 'poseidon', 'role': 'unknown', 'state': 'unknown', 'os': 'unknown', 'tenant': 'vlan1', 'port': 1, 'segment': 'switch1', 'ip': '123.123.123.123', 'mac': '00:00:00:00:00:00', 'hash': 'foo1', 'behavior': 1},
              {'active': 1, 'record_source': 'poseidon', 'role': 'unknown', 'state': 'unknown', 'os': 'unknown', 'tenant': 'vlan1',
                  'port': 1, 'segment': 'switch1', 'ip': '123.123.123.123', 'mac': '00:00:00:00:00:00', 'hash': 'foo2', 'behavior': 1},
@@ -21,4 +20,5 @@ def test_Prometheus():
                  'port': 2, 'segment': 'switch1', 'ip': '2106::1', 'mac': '00:00:00:00:00:00', 'hash': 'foo4', 'behavior': 1},
              {'active': 1, 'record_source': 'poseidon', 'role': 'unknown', 'state': 'unknown', 'os': 'unknown', 'tenant': 'vlan1', 'port': 1, 'segment': 'switch1', 'ip': '::', 'mac': '00:00:00:00:00:00', 'hash': 'foo5', 'behavior': 1}]
     p = Prometheus()
+    p.initialize_metrics()
     p.update_metrics(hosts)
