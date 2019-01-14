7c2f5ce0a80577fe5695f0d1f9fa11c5283bd9c6
cglewis <clewis@iqt.org>
2019-01-14 14:30:33 -0800

    add tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -12,15 +12,26 @@ def test_commands():
     commands.what_is('foo')
     commands.where_is('foo')
     commands.collect_on('foo')
+    commands.show_active('foo')
+    commands.show_inactive('foo')
+    commands.clear_inactives('foo')
+    commands.ignore('foo')
+    commands.show_ignored('foo')
+    commands.remove('foo')
+    commands.show_state('foo')
+    commands.show_devices('foo')
 
 
 def test_poseidonshell():
     shell = PoseidonShell()
     shell.do_record('foo.txt')
     shell.do_what('foo')
+    shell.complete_what('foo', 'what ok yeah', 0, 1)
     shell.do_where('foo')
     shell.do_collect('foo')
     shell.do_quit('foo')
     shell.do_playback('foo.txt')
     shell.precmd('foo')
     shell.close()
+    answer = PoseidonShell.completion('foo', 'what ok yeah', ['yeah'])
+    assert answer == 'yeah'
