e3b091f0bf9368f3f1a82de3dc556bab9287b56e
cglewis <clewis@iqt.org>
2019-01-24 11:04:51 -0800

    extra check

--- poseidon/cli/cli.py
+++ poseidon/cli/cli.py
@@ -72,7 +72,8 @@ class PoseidonShell(cmd.Cmd):
                 args = rest.split(' ', 1)
             else:
                 not_flags.append(args[0])
-                args = args[1].split(' ', 1)
+                if len(args) > 1:
+                    args = args[1].split(' ', 1)
         print('flags: {0}, not flags: {1}'.format(flags, ' '.join(not_flags)))
         return flags, ' '.join(not_flags)
 
