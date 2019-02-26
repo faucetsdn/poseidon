490e2e7f6bd742bc911ba3f310c0f0967e510afa
cglewis <clewis@iqt.org>
2019-02-25 17:02:38 -0800

    fix test

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -11,5 +11,5 @@ def test_commands():
     commands.what_is('foo')
     commands.history_of('foo')
     commands.where_is('foo')
-    commands.show_devices('foo', 'bar')
+    commands.show_devices('foo bar')
     commands.show_devices('all')
