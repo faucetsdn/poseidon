be90b58721bc7890396494b4f5ed9a605f720ec0
cglewis <clewis@iqt.org>
2019-04-10 12:38:16 -0700

    fixes #1152

--- poseidon/cli/cli.py
+++ poseidon/cli/cli.py
@@ -428,7 +428,8 @@ class Parser():
                 for row in records:
                     del row[val]
                 del fields[val]
-            matrix = records
+            if len(fields) > 0:
+                matrix = records
         if not nonzero and not unique:
             for endpoint in endpoints:
                 record = []
