30dc4cb47edb1aac15167759ccf6c8e9ba4cfc2c
Josh Bailey <josh@vandervecken.com>
2019-10-09 15:16:32 +1300

    Need to save exit code.

--- tests/api_smoke_test.sh
+++ tests/api_smoke_test.sh
@@ -4,6 +4,7 @@ SCRIPTPATH=$(readlink -f "$0")
 TESTDIR=`dirname $SCRIPTPATH`
 APIDIR=`readlink -f $TESTDIR/../api`
 PYTHONPATH=$APIDIR timeout -s2 5s gunicorn -b 127.0.0.1:8000 -k gevent -w 1 app.app
-if [ $? -eq 124 ] || [ $? -eq 0 ] ; then echo PASS ; exit 0 ; fi
-echo FAIL: $?
+X=$?
+if [ $X -eq 124 ] || [ $X -eq 0 ] ; then echo PASS ; exit 0 ; fi
+echo FAIL: exit $X
 exit 1
