61db069ac982ba7a485f07e22d7908225e545b43
Ryan Ashley <rashley@iqt.org>
2019-10-19 15:56:09 -0400

    fixing unpack error

--- poseidon/main.py
+++ poseidon/main.py
@@ -719,7 +719,8 @@ class Monitor(object):
                         self.logger.error(
                             'Unable to change endpoint {0} because: {1}'.format(endpoint.name, str(e)))
         elif routing_key == 'poseidon.action.update_acls':
-            for ip, rules in my_obj:
+            for ip in my_obj:
+                rules = my_obj[ip]
                 endpoint = self.s.endpoints_by_ip(ip)
                 if endpoint:
                     try:
--- tests/test_main.py
+++ tests/test_main.py
@@ -202,7 +202,7 @@ def test_format_rabbit_message():
     retval = mockMonitor.format_rabbit_message(message)
     assert retval == {}
 
-    ip_data = dict({'ip':'10.0.01', 'rules':['rule1']})
+    ip_data = dict({'10.0.01':['rule1']})
     message = ('poseidon.action.update_acls', json.dumps(ip_data))
     retval = mockMonitor.format_rabbit_message(message)
     assert retval == {}
