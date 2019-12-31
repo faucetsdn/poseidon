7966346e3bdd575e092f3b669cc9bdc0d2c593ac
cglewis <clewis@iqt.org>
2019-12-31 09:34:26 -0800

    add test file

---
+++ tests/test_worker.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 31 Dec 2019
+@author: cglewis
+"""
+from workers.worker import callback
+from workers.worker import load_workers
+from workers.worker import main
+from workers.worker import setup_docker
+from workers.worker import setup_redis
+
+
+def test_setup_docker():
+    d = setup_docker()
