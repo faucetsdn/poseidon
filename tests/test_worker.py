44407dc860df2c54290fb70e06def37bcdfec7db
cglewis <clewis@iqt.org>
2019-12-31 10:54:24 -0800

    more test coverage

--- tests/test_worker.py
+++ tests/test_worker.py
@@ -34,9 +34,21 @@ def test_callback():
             self.delivery_tag = None
             self.routing_key = ''
 
+    os.environ['VOL_PREFIX'] = '/tmp'
     ch = MockChannel()
     method = MockMethod()
     body = '{"id": "", "type": "metadata", "file_path": "/files/tcprewrite-dot1q-2019-12-31-17_33_32.961111-UTC/pcap-node-splitter-2019-12-31-17_34_17.314910-UTC/clients/trace_5c7820e51dbabbf0476097dda838c7eabfa8e160_2019-12-31_17_18_17-client-ip-38-103-36-98-192-168-0-46-38-103-36-98-eth-udpencap-ip-frame-wsshort-udp-esp-port-4500.pcap", "data": "", "results": {"tool": "p0f", "version": "0.1.7"}}'
     body = body.encode('utf-8')
-    os.environ['VOL_PREFIX'] = '/tmp'
+    callback(ch, method, None, body)
+    body = '{"id": "", "type": "metadata", "file_path": "/files/tcprewrite-dot1q-2019-12-31-17_33_32.961111-UTC/pcap-node-splitter-2019-12-31-17_34_17.314910-UTC/clients/trace_5c7820e51dbabbf0476097dda838c7eabfa8e160_2019-12-31_17_18_17-client-ip-38-103-36-98-192-168-0-46-38-103-36-98-eth-udpencap-ip-frame-wsshort-udp-esp-port-4500.pcap", "data": "foo", "results": {"tool": "p0f", "version": "0.1.7"}}'
+    body = body.encode('utf-8')
+    callback(ch, method, None, body)
+    body = '{"id": "", "type": "metadata", "file_path": "/files/tcprewrite-dot1q-2019-12-31-17_33_32.961111-UTC/pcap-node-splitter-2019-12-31-17_34_17.314910-UTC/clients/trace_5c7820e51dbabbf0476097dda838c7eabfa8e160_2019-12-31_17_18_17-client-ip-38-103-36-98-192-168-0-46-38-103-36-98-eth-udpencap-ip-frame-wsshort-udp-esp-port-4500.pcap", "data": "", "results": {"tool": "ncapture", "version": "0.1.7"}}'
+    body = body.encode('utf-8')
+    callback(ch, method, None, body)
+    body = '{"id": "", "type": "data", "file_path": "/files/tcprewrite-dot1q-2019-12-31-17_33_32.961111-UTC/pcap-node-splitter-2019-12-31-17_34_17.314910-UTC/clients/trace_5c7820e51dbabbf0476097dda838c7eabfa8e160_2019-12-31_17_18_17-client-ip-38-103-36-98-192-168-0-46-38-103-36-98-eth-udpencap-ip-frame-wsshort-udp-esp-port-4500.pcap", "data": "", "results": {"tool": "p0f", "version": "0.1.7"}}'
+    body = body.encode('utf-8')
+    callback(ch, method, None, body)
+    body = '{"type": "data", "file_path": "/files/tcprewrite-dot1q-2019-12-31-17_33_32.961111-UTC/pcap-node-splitter-2019-12-31-17_34_17.314910-UTC/clients/trace_5c7820e51dbabbf0476097dda838c7eabfa8e160_2019-12-31_17_18_17-client-ip-38-103-36-98-192-168-0-46-38-103-36-98-eth-udpencap-ip-frame-wsshort-udp-esp-port-4500.pcap", "data": "", "results": {"tool": "p0f", "version": "0.1.7"}}'
+    body = body.encode('utf-8')
     callback(ch, method, None, body)
