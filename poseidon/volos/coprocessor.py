62216d0cd1103bbc608145e6c1bdca67478a9268
ryan <rashley@iqt.org>
2020-02-13 15:02:18 -0500

    coprocessor to start/stop Pipette

--- poseidon/volos/coprocessor.py
+++ poseidon/volos/coprocessor.py
@@ -3,108 +3,112 @@
 Created on 30 January 2020
 @author: Ryan Ashley
 """
-import ast
+import git
 import json
 import logging
+import os
+import subprocess
 
-import requests
-
-from poseidon.helpers.config import Config
-
+from pathlib import Path
 
 class Coprocessor(object):
 
-    def __init__(self, endpoint, switch, iterations=1):
+    def __init__(self, controller):
         self.logger = logging.getLogger('coprocessor')
-        self.controller = Config().get_config()
-        self.endpoint = endpoint
-        self.id = endpoint.name
-        self.mac = endpoint.endpoint_data['mac']
-        nic = self.controller['coprocessor_nic']
+        self.pipette_repo = controller['pipette_repo']
+        self.pipette_dir = controller['pipette_dir']
+        self.coprocessor_nic = controller['coprocessor_nic']
+        self.coprocessor_port = controller['coprocessor_port']
+        self.coprocessor_vlan = controller['coprocessor_vlan']
+        self.fake_interface = controller['fake_interface']
+        self.fake_mac = controller['fake_mac']
+        self.fake_server_mac = controller['fake_server_mac']
+        self.fake_ip = controller['fake_ip']
+        self.bridge = controller['bridge']
+        self.pipette_port = controller['pipette_port']
+        self.pcap_location = controller['pcap_location']
+        self.pcap_size = controller['pcap_size']
+
+    def update_pipette(self):
+        status = True
         try:
-            eval_nic = ast.literal_eval(nic)
-            if switch in eval_nic:
-                self.nic = eval_nic[switch]
-            else:
-                self.logger.error(
-                    'Failed to get coprocessor nic for the switch: {0}'.format(switch))
-        except ValueError:
-            self.nic = nic
-        self.interval = str(self.controller['reinvestigation_frequency'])
-        self.iterations = str(iterations)
+            if not os.path.exists(self.pipette_dir):
+                Path(self.pipette_dir).mkdir(parents=True, exist_ok=True)
+        except Exception as e:  # pragma: no cover
+            self.logger.error('Pipette directory:{0} could not be created. Coprocessing may not work as expected. {1}'
+                .format(self.pipette_dir, str(e)))
+            status = False
+        if status :
+            try:
+                repo = git.Repo(self.pipette_dir)
+            except git.exc.InvalidGitRepositoryError: # pragma: no cover
+                repo = git.Repo.clone_from(self.pipette_repo, self.pipette_dir)
+            if repo and not repo.bare :
+                repo.remotes.origin.pull()
+
+        return status
+    def start_pipette(self):
+        '''
+        pipette params:
+        -c,  coproint      interface to send coprocessed traffic to
+        -f,  fakeint       interface created for fake services to run on
+        -m,  fakemac       fake mac for fake interface
+        -fch, fakeclientmac fake client mac address
+        -i,  fakeip        fake ip for fake services(will be proxied from real IPS)
+        -b,  bridge        name of ovs bridge to create
+        -p,  port          pipette port
+        -r,  record
+        '''
+        pipette_script = os.path.join(self.pipette_dir, "runpipette.sh")
+        params = [
+            pipette_script,
+            f"-c {self.coprocessor_nic}",
+            f"-f {self.fake_interface}",
+            f"-m {self.fake_mac}",
+            f"-fch {self.fake_server_mac}",
+            f"-i {self.fake_ip}",
+            f"-b {self.bridge}",
+            f"-p {self.pipette_port}",
+            f"-r {self.pcap_location} {self.pcap_size}",
+            ]
+        process = subprocess.Popen(params, 
+                           #stdout=subprocess.PIPE,
+                           #stderr=subprocess.PIPE,
+                           universal_newlines=True)
+
+    def stop_pipette(self):
+        '''
+        pipette params:
+        -c,  coproint      interface to send coprocessed traffic to
+        -f,  fakeint       interface created for fake services to run on
+        -b,  bridge        name of ovs bridge to create"
+        '''
+        pipette_script = os.path.join(self.pipette_dir, "shutdownpipette.sh")
+        params = [
+            pipette_script,
+            f"-c {self.coprocessor_nic}",
+            f"-f {self.fake_interface}",
+            f"-b {self.bridge}",
+            ]
+        process = subprocess.Popen(params, 
+                           stdout=subprocess.PIPE,
+                           stderr=subprocess.PIPE,
+                           universal_newlines=True)
 
     def start_coprocessor(self):
         '''
-        Starts coprocessor for a given endpoint with the
-        options passed in at the creation of the class instance.
+        Starts OVS and containers
         '''
         status = False
-        payload = {
-            'nic': self.nic,
-            'id': self.id,
-            'interval': self.interval,
-            'filter': '\'ether host {0}\''.format(self.mac),
-            'iters': self.iterations,
-            'metadata': "{'endpoint_data': " + str(self.endpoint.endpoint_data) + '}'}
-
-        self.logger.debug('Payload: {0}'.format(str(payload)))
-
-        network_tap_addr = self.controller['network_tap_ip'] + \
-            ':' + self.controller['network_tap_port']
-        uri = 'http://' + network_tap_addr + '/create'
-
-        try:
-            resp = requests.post(uri, data=json.dumps(payload))
-            # TODO improve logged output
-            self.logger.debug(
-                'Collector response: {0}'.format(resp.text))
-            response = ast.literal_eval(resp.text)
-            if response[0]:
-                self.logger.info(
-                    'Successfully started the collector for: {0}'.format(self.id))
-                self.endpoint.endpoint_data['container_id'] = response[1].rsplit(
-                    ':', 1)[-1].strip()
-                status = True
-            else:
-                self.logger.error(
-                    'Failed to start collector because: {0}'.format(response[1]))
-        except Exception as e:  # pragma: no cover
-            self.logger.error(
-                'Failed to start collector because: {0}'.format(str(e)))
+        
         return status
 
     def stop_coprocessor(self):
         '''
-        Stops coprocessor for a given endpoint.
+        Stops OVS and containers
         '''
         status = False
-        if 'container_id' not in self.endpoint.endpoint_data:
-            self.logger.warning(
-                'No collector to stop because no container_id for endpoint')
-            return True
-
-        payload = {'id': [self.endpoint.endpoint_data['container_id']]}
-        self.logger.debug('Payload: {0}'.format(str(payload)))
-
-        network_tap_addr = self.controller['network_tap_ip'] + \
-            ':' + self.controller['network_tap_port']
-        uri = 'http://' + network_tap_addr + '/stop'
-
-        try:
-            resp = requests.post(uri, data=json.dumps(payload))
-            self.logger.debug(
-                'Collector response: {0}'.format(resp.text))
-            response = ast.literal_eval(resp.text)
-            if response[0]:
-                self.logger.info(
-                    'Successfully stopped the collector for: {0}'.format(self.id))
-                status = True
-            else:
-                self.logger.error(
-                    'Failed to stop collector because response failed with: {0}'.format(response[1]))
-        except Exception as e:  # pragma: no cover
-            self.logger.error(
-                'Failed to stop collector because: {0}'.format(str(e)))
+        
         return status
 
     
