32df8e84cd098dcf32df0dba01276862345b7d7a
Josh Bailey <josh@vandervecken.com>
2020-06-26 10:07:15 +1200

    shell quotes.

--- helpers/faucet/certstrap/gen_faucetconfrpc_keys.sh
+++ helpers/faucet/certstrap/gen_faucetconfrpc_keys.sh
@@ -10,8 +10,8 @@ if [ ! -d "$KEYDIR" ] ; then mkdir -p "$KEYDIR" ; fi
 if [ -f "$KEYDIR/$CA.crt" ] ; then exit 0 ; fi
 
 CS="certstrap --depot-path $KEYDIR"
-$CS init --common-name $CA --passphrase ""
+$CS init --common-name "$CA" --passphrase ""
 $CS request-cert --common-name client --passphrase ""
-$CS sign client --CA $CA
-$CS request-cert --common-name $HOST --passphrase ""
-$CS sign $HOST --CA $CA
+$CS sign client --CA "$CA"
+$CS request-cert --common-name "$HOST" --passphrase ""
+$CS sign "$HOST" --CA "$CA"
