380f560bc9a8df2a715a2b3b7a437f62fd193d7d
Josh Bailey <josh@vandervecken.com>
2020-09-25 13:55:29 +1200

    Use dnspython so can tune timeout and access future async resolver.

--- poseidon/helpers/metadata.py
+++ poseidon/helpers/metadata.py
@@ -3,11 +3,13 @@
 Created on 19 February 2019
 @author: Charlie Lewis
 """
-import socket
+import functools
+import dns.resolver
 
 from poseidon.constants import NO_DATA
 
 
+@functools.lru_cache()
 def get_ether_vendor(mac, lookup_path):
     """
     Takes a MAC address and looks up and returns the vendor for it.
@@ -22,16 +24,14 @@ def get_ether_vendor(mac, lookup_path):
         return NO_DATA
 
 
-def get_rdns_lookup(ip):
+def get_rdns_lookup(ip, timeout=5):
     """
     Takes an IP address adn looks up what the reverse DNS is if it exists.
     """
     try:
-        rdns = socket.gethostbyaddr(ip)
-        if rdns[0] == rdns[2][0]:
-            rdns = NO_DATA
-        else:
-            rdns = rdns[0]
-    except Exception as e:  # pragma: no cover
+        resolver = dns.resolver.Resolver()
+        resolver.timeout = timeout
+        resolver.lifetime = resolver.lifetime
+        return str(resolver.resolve_address(ip).canonical_name)
+    except (AttributeError, dns.resolver.NoNameservers, dns.resolver.NXDOMAIN, dns.exception.Timeout):  # pragma: no cover
         return NO_DATA
-    return rdns
