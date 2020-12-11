b1584f2a0e3a9d35b60b15c7f6c9070894a6bbe9
cglewis <clewis@iqt.org>
2020-12-11 12:16:16 -0800

    more logging

--- poseidon/monitor.py
+++ poseidon/monitor.py
@@ -354,6 +354,7 @@ class Monitor:
                                             ep.metadata['mac_addresses'] = {}
                                         self.logger.debug(f'updating endpoint: {ep.name}, {ep.metadata}')
                                         self.logger.debug(f'{type(message)}, {message}')
+                                        self.logger.debug(f'{message["source_mac"]}')
                                         ep.metadata['mac_addresses'][message['source_mac']] = message
                                         updates = True
                 if updates:
