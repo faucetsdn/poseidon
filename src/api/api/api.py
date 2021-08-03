0d47457a2965c4722c0c49736ecc124942e815ba
cglewis <clewis@iqt.org>
2021-08-03 12:14:07 -0700

    remove eventlet, greenlet, and gunicorn - use bjoern

--- src/api/api/api.py
+++ src/api/api/api.py
@@ -1,3 +1,7 @@
+import argparse
+import os
+
+import bjoern
 import falcon
 from falcon_cors import CORS
 
@@ -5,9 +9,20 @@ from .routes import routes
 from .routes import version
 
 
-cors = CORS(allow_all_origins=True)
-api = application = falcon.App(middleware=[cors.middleware])
+def main():
+    cors = CORS(allow_all_origins=True)
+    api = application = falcon.App(middleware=[cors.middleware])
 
-r = routes()
-for route in r:
-    api.add_route(version()+route, r[route])
+    r = routes()
+    for route in r:
+        api.add_route(version()+route, r[route])
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--port', '-p', help='Port to run the API webserver on', type=int, default=8000)
+    parser.add_argument(
+        '--prom_addr', '-a', help='Prometheus address connected to Poseidon, i.e. "prometheus:9090"', default='prometheus:9090')
+    args = parser.parse_args()
+
+    os.environ['PROM_ADDR'] = args.prom_addr
+    bjoern.run(api, '0.0.0.0', args.port)
