86306225838b5afcb35b96507553858fc2b2182c
cglewis <clewis@iqt.org>
2021-08-03 12:52:04 -0700

    remove gunicorn from tests

--- tests/api_smoke_test.sh
+++ tests/api_smoke_test.sh
@@ -3,7 +3,8 @@
 SCRIPTPATH=$(readlink -f "$0")
 TESTDIR=$(dirname $SCRIPTPATH)
 APIDIR=$(readlink -f $TESTDIR/../src/api)
-PYTHONPATH=$APIDIR timeout -s2 5s gunicorn -b 127.0.0.1:8000 -k eventlet -w 1 api.api
+pushd $APIDIR && python3 setup.py install && popd
+PYTHONPATH=$APIDIR timeout -s2 5s poseidon-api
 X=$?
 if [ $X -eq 124 ] || [ $X -eq 0 ] ; then echo PASS ; exit 0 ; fi
 echo FAIL: exit $X
